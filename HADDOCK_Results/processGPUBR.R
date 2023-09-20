b16272ecb1eae4d53a84926d5f61d3379f8649d5
Colby T. Ford <colby@tuple.xyz>
2023-09-20 11:48:31 -0400

    Update processGPUBR.R

--- HADDOCK_Results/processGPUBR.R
+++ HADDOCK_Results/processGPUBR.R
@@ -1,17 +1,31 @@
-# Load data
-library("ggplot2")
-library("ggpubr")
-df <- read.csv("/Users/syasa/Desktop/Research/BA.2.86/HADDOCK_Results.csv")
-variant_comparisons <- combn(c("BA.1", "BA.2", "BA.2.86", "XBB.1.5","WT"), 2, simplify = FALSE)
-had_boxplot <- ggboxplot(df, x = "Variant",
+## Load data
+library(dplyr)
+library(ggplot2)
+library(ggpubr)
+
+# df <- read.csv("/Users/syasa/Desktop/Research/BA.2.86/HADDOCK_Results.csv")
+df <- read.csv("HADDOCK_Results.csv")
+variant_comparisons <- combn(c("WT", "XBB.1.5", "BA.2", "BA.1", "BA.2.86"), 2, simplify = FALSE)
+
+had_boxplot <- ggboxplot(df,
+                         x = "Variant",
                          y = "HADDOCK.score",
                          ylab="HADDOCK Score",
                          xlab = "Variant",
                          color = "Variant",
-                         palette =c("#00AFBB", "#E7B800", "#FC4E07", "#ADD8E6","#000000"),
-                         add = "dotplot")+ 
-  stat_compare_means(method = "kruskal.test", comparisons = variant_comparisons)
+                         palette = c("#00AFBB", "#E7B800", "#FC4E07", "#ADD8E6","#000000"),
+                         add = "dotplot") + 
+  stat_compare_means(comparisons = variant_comparisons) +
+  stat_compare_means(label.y = -150)
+  
 had_boxplot
 
 variant_comparisons
 
+
+
+kruskal.test(HADDOCK.score ~ Variant, data = df %>% filter(Variant %in% c("WT", "BA.2.86")))
+
+
+pairwise.wilcox.test(df$HADDOCK.score, df$Variant,
+                     p.adjust.method = "BH")
